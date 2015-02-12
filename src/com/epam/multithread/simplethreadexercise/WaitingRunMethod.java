bb78e57937ad32e95b943ea545ce6be48b2f2150
Peter Horvath <peter.horvath.2005@gmail.com>
2015-02-12 10:00:45 +0100

    add WaitingRunMethod

---
+++ src/com/epam/multithread/simplethreadexercise/WaitingRunMethod.java
@@ -0,0 +1,18 @@
+package com.epam.multithread.simplethreadexercise;
+
+public class WaitingRunMethod {
+	private final static int WAIT_TIME = 1000;
+
+	public void run() {
+		System.out.println("WaitingRunMethod started and waits for "
+				+ WAIT_TIME / 1000 + " second(s)");
+		try {
+			Thread.sleep(WAIT_TIME);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println("WaitingRunMethod ended.");
+		}
+
+	}
+}
